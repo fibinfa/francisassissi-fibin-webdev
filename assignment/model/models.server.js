86e9d7a914cf9a5d9295a5bc34d362f11b1dfcc1
fibinfa <francisassissi.f@husky.neu.edu>
2017-03-22 23:25:39 -0400

    mongo connectivity

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,6 +1,14 @@
 module.exports =function() {
      var mongoose = require('mongoose');
-     mongoose.connect('mongodb://localhost/webappmaker');
+     var connectionString='mongodb://localhost/webappmaker';
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+     mongoose.connect(connectionString);
 
      var userModel = require("./user/user.model.server")();
      var websiteModel = require("./website/website.model.server")();
